17aaa7d3e46ddd13a7d1b211f2872636700515fd
Stella <stella.iemma@gmail.com>
2021-02-01 19:18:30 +0000

    💅

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './Home.css';
+import FadeIn from "react-fade-in";
+import "bootstrap/dist/css/bootstrap.css";
 
 export const Home: React.FunctionComponent = () => {
   const homeURLs = {
@@ -7,11 +10,29 @@ export const Home: React.FunctionComponent = () => {
     github: "https://github.com/stehzitcha/"
   }
 
-  return (
+const [loading, setLoading] = useState(true)
+
+useEffect(() => {
+    setTimeout(() => {
+      const el = document.querySelector(".loader-container");
+      if (el) {
+        el.remove();
+        setLoading(!loading);
+      }
+    }, 2500)
+    
+}, []);
+
+if (loading) {
+    return null;
+}
+
+return (
+  <FadeIn>
     <div className="container centered">
       <header id="header">
         <h1>Stella Iemma</h1>
-        <p>Crazy human being and software developer on the spare time </p>
+        <p>Crazy human being and software developer on spare time </p>
         <nav>
           <ul>
             <li><a href={homeURLs.github}><i className="nes-icon github is-large"></i><span className="label">Github</span></a></li>
@@ -21,5 +42,6 @@ export const Home: React.FunctionComponent = () => {
         </nav>
       </header>
     </div>
+  </FadeIn>
   );
 }
